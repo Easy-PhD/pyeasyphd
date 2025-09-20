c1b1aa1532534856f67c96f2fcd2a6cff244340e
NextAI <nextartifintell@users.noreply.com>
2025-09-20 13:47:04 +0800

    feat(data/): add data/

--- pyeasyphd/main/basic_input.py
+++ pyeasyphd/main/basic_input.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any, Dict
 
-from pyadvtools import read_list, standard_path
+from pyadvtools import read_list
 from pybibtexer.main import BasicInput as BasicInputInPyBibtexer
 
 
@@ -12,12 +12,11 @@ class BasicInput(BasicInputInPyBibtexer):
         options (Dict[str, Any]): Options.
 
     Attributes:
-        path_bibs (str): Path bibs.
-        path_figures (str): Path figures.
-        path_templates (str): Path templates.
-
+        full_json_c (str):
+        full_json_j (str):
         full_csl_style_pandoc (str): Full path to csl style for pandoc.
         full_tex_article_template_pandoc (str): Full path to tex article template for pandoc.
+        full_tex_beamer_template_pandoc (str): Full path to tex beamer template for pandoc.
         article_template_tex (List[str]): Article template for LaTex.
 
         article_template_header_tex (List[str]): Article template header for LaTex.
@@ -32,39 +31,19 @@ class BasicInput(BasicInputInPyBibtexer):
     """
 
     def __init__(self, options: Dict[str, Any]) -> None:
-        # The paths of Figures, Bibs, and Templates
-        self.path_bibs: str = standard_path(options.get("path_bibs", ""))
-        self.path_figures: str = standard_path(options.get("path_figures", ""))
-        self.path_templates: str = standard_path(options.get("path_templates", ""))
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self._path_templates = os.path.join(os.path.dirname(current_dir), "data", "Templates")
 
-        # Update
-        path_config = standard_path(options.get("path_config", ""))
-        if len(self.path_bibs) == 0:
-            for folder in [
-                "bib", "bibs", "Bib", "Bibs", "BIB", "BIBS",
-                "reference", "references", "Reference", "References", "REFERENCE", "REFERENCES"
-            ]:
-                if os.path.exists(p := os.path.join(path_config, folder)):
-                    self.path_bibs = p
-                    break
+        full_json_c = os.path.join(self._path_templates, "AbbrFull", "conferences.json")
+        full_json_j = os.path.join(self._path_templates, "AbbrFull", "journals.json")
 
-        if len(self.path_figures) == 0:
-            for folder in [
-                "figure", "figures", "Figure", "Figures", "FIGURE", "FIGURES",
-                "fig", "figs", "Fig", "Figs", "FIG", "FIGS",
-            ]:
-                if os.path.exists(p := os.path.join(path_config, folder)):
-                    self.path_figures = p
-                    break
+        _full_json_c = options.get("full_json_c")
+        if isinstance(_full_json_c, "str"):
+            full_json_c = _full_json_c
+        _full_json_j = options.get("full_json_j")
+        if isinstance(_full_json_j, "str"):
+            full_json_j = _full_json_j
 
-        if len(self.path_templates) == 0:
-            for folder in ["template", "templates", "Template", "Templates", "TEMPLATE", "TEMPLATES"]:
-                if os.path.exists(p := os.path.join(path_config, folder)):
-                    self.path_templates = p
-                    break
-
-        full_json_c = os.path.join(self.path_templates, "AbbrFull", "conferences.json")
-        full_json_j = os.path.join(self.path_templates, "AbbrFull", "journals.json")
         super().__init__(full_json_c, full_json_j, options)
 
         # main
@@ -76,28 +55,24 @@ class BasicInput(BasicInputInPyBibtexer):
     # main
     def _initialize_pandoc_md_to(self, options: Dict[str, Any]) -> None:
         csl_name = options.get("csl_name", "apa-no-ampersand")
-        if len(csl_name) == 0:
+        if not isinstance(csl_name, str):
             csl_name = "apa-no-ampersand"
+        self.full_csl_style_pandoc = os.path.join(self._path_templates, "CSL", f"{csl_name}.csl")
+        if not os.path.exists(self.full_csl_style_pandoc):
+            self.full_csl_style_pandoc = os.path.join(self._path_templates, "CSL", "apa-no-ampersand.csl")
 
-        full_csl_style_pandoc = os.path.join(self.path_templates, "CSL", f"{csl_name}.csl")
-        if (p := options.get("full_csl")) is not None:
-            full_csl_style_pandoc = p
-        self.full_csl_style_pandoc = full_csl_style_pandoc
+        self.full_tex_article_template_pandoc = os.path.join(self._path_templates, "TEX", "eisvogel.latex")
+        self.full_tex_beamer_template_pandoc = os.path.join(self._path_templates, "TEX", "eisvogel.beamer")
 
-        full_tex_article_template_pandoc = os.path.join(self.path_templates, "TEX", "eisvogel.tex")
-        if (p := options.get("full_eisvogel")) is not None:
-            full_tex_article_template_pandoc = p
-        self.full_tex_article_template_pandoc = full_tex_article_template_pandoc
-
-        self.article_template_tex = self._try_read_list(options, "TEX", "Article.tex", "full_article")
+        self.article_template_tex = self._try_read_list("TEX", "Article.tex")
 
     def _initialize_python_run_tex(self, options: Dict[str, Any]) -> None:
-        self.article_template_header_tex = self._try_read_list(options, "TEX", "Article_Header.tex", "full_article_header")
-        self.article_template_tail_tex = self._try_read_list(options, "TEX", "Article_Tail.tex", "full_article_tail")
-        self.beamer_template_header_tex = self._try_read_list(options, "TEX", "Beamer_Header.tex", "full_beamer_header")
-        self.beamer_template_tail_tex = self._try_read_list(options, "TEX", "Beamer_Tail.tex", "full_beamer_tail")
-        self.math_commands_tex = self._try_read_list(options, "TEX", "math_commands.tex", "full_math_commands")
-        self.usepackages_tex = self._try_read_list(options, "TEX", "Style.tex", "full_usepackages_tex")
+        self.article_template_header_tex = self._try_read_list("TEX", "Article_Header.tex")
+        self.article_template_tail_tex = self._try_read_list("TEX", "Article_Tail.tex")
+        self.beamer_template_header_tex = self._try_read_list("TEX", "Beamer_Header.tex")
+        self.beamer_template_tail_tex = self._try_read_list("TEX", "Beamer_Tail.tex")
+        self.math_commands_tex = self._try_read_list("TEX", "math_commands.tex")
+        self.usepackages_tex = self._try_read_list("TEX", "Style.tex")
 
         # handly preamble
         self.handly_preamble = options.get("handly_preamble", False)
@@ -106,12 +81,8 @@ class BasicInput(BasicInputInPyBibtexer):
             self.beamer_template_header_tex, self.beamer_template_tail_tex = [], []
             self.math_commands_tex, self.usepackages_tex = [], []
 
-    def _try_read_list(self, options: Dict[str, Any], folder_name: str, file_name: str, key: str):
-        path_file = os.path.join(self.path_templates, folder_name, file_name)
-        if (p := options.get(key)) is None:
-            return []
-        else:
-            path_file = p
+    def _try_read_list(self, folder_name: str, file_name: str):
+        path_file = os.path.join(self._path_templates, folder_name, file_name)
 
         try:
             data_list = read_list(path_file)
