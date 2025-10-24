cc77bf06383dfad68083da2f2fcbbf1848b3d8b3
NextAI <nextartifintell@users.noreply.com>
2025-10-24 09:41:20 +0800

    feat(examples/): add examples/

---
+++ examples/local_config.py
@@ -0,0 +1,12 @@
+import os
+
+local_options = {
+    "path_spidered_bibs": os.path.expanduser(""),
+    "path_spidering_bibs": os.path.expanduser(""),
+    "path_conf_j_jsons": os.path.expanduser(""),
+    "path_output": os.path.expanduser(""),
+    "zotero_bib": os.path.expanduser(""),  # "MyLibrary.bib" exported from Zotero
+    "path_weekly_docs": os.path.expanduser(""),
+    "path_monthly_docs": os.path.expanduser(""),
+    "path_yearly_docs": os.path.expanduser("")
+}
