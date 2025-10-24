745e03b61763047d3a97ccb72201d8836d9a9f9b
NextAI <nextartifintell@users.noreply.com>
2025-10-24 10:13:06 +0800

    feat(scripts/): update scripts/

--- pyeasyphd/scripts/__init__.py
+++ pyeasyphd/scripts/__init__.py
@@ -14,7 +14,8 @@ __all__ = [
     "run_compare_bib_with_local",
     "run_compare_bib_with_zotero",
     "run_format_bib_to_save_by_entry_type",
-    "run_format_bib_to_abbr_zotero_save"
+    "run_format_bib_to_abbr_zotero_save",
+    "run_replace_to_standard_cite_keys"
 ]
 
 from .run_article_md import run_article_md_daily_notes
@@ -29,4 +30,5 @@ from .run_generate import (
     run_generate_j_weekly,
     run_generate_j_yearly,
 )
+from .run_replace import run_replace_to_standard_cite_keys
 from .run_search import run_compare_after_search, run_search_for_files, run_search_for_screen
