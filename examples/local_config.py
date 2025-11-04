a060ca2fc5f9e5067c07dcdaeab1147a39d215ae
NextAI <nextartifintell@users.noreply.com>
2025-11-04 21:02:48 +0800

    style(examples/): update examples/

--- examples/local_config.py
+++ examples/local_config.py
@@ -1,12 +1,17 @@
 import os
 
+
+def update_path(path_input: str):
+    return os.path.expandvars(os.path.expanduser(path_input))
+
+
 local_options = {
-    "path_spidered_bibs": os.path.expanduser(""),
-    "path_spidering_bibs": os.path.expanduser(""),
-    "path_conf_j_jsons": os.path.expanduser(""),
-    "path_output": os.path.expanduser(""),
-    "zotero_bib": os.path.expanduser(""),  # "MyLibrary.bib" exported from Zotero
-    "path_weekly_docs": os.path.expanduser(""),
-    "path_monthly_docs": os.path.expanduser(""),
-    "path_yearly_docs": os.path.expanduser(""),
+    "path_spidered_bibs": update_path(""),
+    "path_spidering_bibs": update_path(""),
+    "path_conf_j_jsons": update_path(""),
+    "path_output": update_path(""),
+    "zotero_bib": update_path(""),  # "MyLibrary.bib" exported from Zotero
+    "path_weekly_docs": update_path(""),
+    "path_monthly_docs": update_path(""),
+    "path_yearly_docs": update_path(""),
 }
